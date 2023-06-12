bc5e1928943ccba047fb223e2950381173b4ffd4
AleWrk <129170310+AleWrk@users.noreply.github.com>
2023-06-12 13:09:19 +0000

    settings and apply

--- code/pages/00_Chat.py
+++ code/pages/00_Chat.py
@@ -3,17 +3,25 @@ from streamlit_chat import message
 import os
 from utilities.helper import LLMHelper
 
+
+llm_helper = LLMHelper()
+
+def applycnfg(toklen,tmpe):
+    #print(toklen)
+    llm_helper = LLMHelper(temperature=tmpe, max_tokens=toklen)
+
 col1, col2, col3 = st.columns([2, 2, 2])
 with col3:
     with st.expander("Settings"):
-        st.tokens_response = st.slider(
+        toklen=st.tokens_response = st.slider(
             "Tokens response length", 100, 1000, 500)
-        st.slider("Temperature", min_value=0.0, max_value=1.0,
-                    step=0.1, key='custom_temperature')
+        tmpe=st.slider("Temperature", min_value=0.0, max_value=1.0,
+                    step=0.1)
         #st.text_area("Custom Prompt", key='custom_prompt', on_change=check_variables_in_prompt,
         #             placeholder=custom_prompt_placeholder, help=custom_prompt_help, height=150)
         #st.selectbox("Language", [
         #                None] + list(available_languages.keys()), key='translation_language')
+        aplychng = st.button("Apply", key="apply_chat", on_click=applycnfg(toklen,tmpe))
 
 def clear_text_input():
     st.session_state['question'] = st.session_state['input']
@@ -35,6 +43,8 @@ if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []
 if 'source_documents' not in st.session_state:
     st.session_state['source_documents'] = []
+
+
 
 def search_from_data():
     st.session_state['question'] = valueent
@@ -59,7 +69,7 @@ def search_from_data():
     
 
 
-llm_helper = LLMHelper()
+
 
 # Chat 
 #st.text_input("You: ", placeholder="type your question", key="input", on_change=clear_text_input)
